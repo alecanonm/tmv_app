74e72027ef1da226a35c87bfd7a9ab737e508728
alecanonm <al3jandrocan0n@gmail.com>
2023-12-01 17:29:14 -0500

    enhancing tah

--- components/atoms/Tag.jsx
+++ components/atoms/Tag.jsx
@@ -5,7 +5,7 @@ const Tag = ({ price }) => {
   return (
     <figure className='fixed top-20 right-0 sm:right-5 z-50'>
       <Image src={tag} alt='price' className='w-[80px] sm:w-28' />
-      <p className='fixed  text-[#153414] top-[106px] sm:top-[118px] sm:right-[47px] right-[22px] -rotate-45 text-lg md:text-2xl'>
+      <p className='fixed  text-[#153414] top-[96px] sm:top-[107px] sm:right-[57px] right-[25px] -rotate-45 text-lg md:text-2xl'>
         <span>
           <strong>{price}</strong>
         </span>
@@ -13,7 +13,7 @@ const Tag = ({ price }) => {
           <strong>€</strong>
         </span>
       </p>
-      <p className='fixed  text-[#153414] pt-2 top-[110px] sm:top-[130px] sm:right-[50px] right-[22px] -rotate-45  '>
+      <p className='fixed  text-[#153414] pt-3 top-[102px] sm:top-[120px] sm:right-[55px] right-[24px] -rotate-45  '>
         <strong>c/u</strong>
       </p>
     </figure>
