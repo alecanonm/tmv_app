8550303446adebec704189af43e02cd389da6ada
Jair Cañon <jhcanonn@gmail.com>
2023-12-03 15:46:42 -0500

    Feature: Adding total price to Table

--- components/molecules/OrderTable.jsx
+++ components/molecules/OrderTable.jsx
@@ -1,9 +1,9 @@
 import fallbacklImage from '@public/assets/vaper-logo.png'
+import trash from '@public/assets/delete.png'
 import { ImageWithFallback } from '@components/atoms'
 import { useVapesContext } from '@contexts/VapesContext'
 import { getCurrencyEUR } from '@utils'
 import { useParams } from 'next/navigation'
-import trash from '@public/assets/delete.png'
 import { LS_VAPES_TO_BOX, setLocalStorage, LS_GLOBAL_COUNTERS } from '@utils'
 
 const OrderTable = () => {
@@ -37,67 +37,77 @@ const OrderTable = () => {
   }
 
   return (
-    <div className='overflow-x-auto w-full'>
-      <table className='text-black w-full min-w-[30rem]'>
-        <thead>
-          <tr>
-            <th className='bg-brandGrayBG p-2 rounded-l-lg'>Imagen</th>
-            <th className='bg-brandGrayBG p-2'>Nombre</th>
-            <th className='bg-brandGrayBG p-2'>Cantidad</th>
-            <th className='bg-brandGrayBG p-2'>Precio</th>
-            <th className='bg-brandGrayBG p-2'>Total</th>
-            <th className='bg-brandGrayBG p-2 rounded-r-lg'></th>
-          </tr>
-        </thead>
-        <tbody className='text-center'>
-          {vapesToBox
-            .filter((vape) => vape.brand.id === brandId)
-            .map((vape) => {
-              const imageId = vape.images[0].vapes_images_id.image?.id
-              const imageUrl = `${process.env.NEXT_PUBLIC_DIRECTUS_BASE_URL}assets/${imageId}`
-              const qty = vape.quantity
-              const flavor = vape.flavor.name
-              return (
-                qty > 0 && (
-                  <tr key={vape.id}>
-                    <td className='min-w-[6rem]'>
-                      <ImageWithFallback
-                        src={imageId ? imageUrl : fallbacklImage}
-                        alt={flavor}
-                        width={55}
-                        height={55}
-                      />
-                    </td>
-                    <td className='min-w-[10rem]'>
-                      <strong>{flavor}</strong>
-                    </td>
-                    <td>{qty}</td>
-                    <td className='min-w-[6rem]'>
-                      {getCurrencyEUR(vape.price)}
-                    </td>
-                    <td className='min-w-[6rem]'>
-                      {getCurrencyEUR(qty * vape.price)}
-                    </td>
-                    <td className='min-w-[3rem]'>
-                      <button
-                        role='button'
-                        type='button'
-                        onClick={() => handleDelete(vape.id)}
-                      >
+    <div className='flex flex-col gap-2 w-full justify-center'>
+      <div className='overflow-x-auto w-full'>
+        <table className='text-black w-full min-w-[30rem]'>
+          <thead>
+            <tr>
+              <th className='bg-brandGrayBG p-2 rounded-l-lg'>Imagen</th>
+              <th className='bg-brandGrayBG p-2'>Nombre</th>
+              <th className='bg-brandGrayBG p-2'>Cantidad</th>
+              <th className='bg-brandGrayBG p-2'>Precio</th>
+              <th className='bg-brandGrayBG p-2'>Total</th>
+              <th className='bg-brandGrayBG p-2 rounded-r-lg'></th>
+            </tr>
+          </thead>
+          <tbody className='text-center'>
+            {vapesToBox
+              .filter((vape) => vape.brand.id === brandId)
+              .map((vape) => {
+                const imageId = vape.images[0].vapes_images_id.image?.id
+                const imageUrl = `${process.env.NEXT_PUBLIC_DIRECTUS_BASE_URL}assets/${imageId}`
+                const qty = vape.quantity
+                const flavor = vape.flavor.name
+                return (
+                  qty > 0 && (
+                    <tr key={vape.id}>
+                      <td className='min-w-[6rem]'>
                         <ImageWithFallback
-                          src={trash}
-                          alt='delete'
-                          width={20}
-                          height={20}
+                          src={imageId ? imageUrl : fallbacklImage}
+                          alt={flavor}
+                          width={55}
+                          height={55}
                         />
-                      </button>
-                    </td>
-                  </tr>
+                      </td>
+                      <td className='min-w-[10rem]'>
+                        <strong>{flavor}</strong>
+                      </td>
+                      <td>{qty}</td>
+                      <td className='min-w-[6rem]'>
+                        {getCurrencyEUR(vape.price)}
+                      </td>
+                      <td className='min-w-[6rem]'>
+                        {getCurrencyEUR(qty * vape.price)}
+                      </td>
+                      <td className='min-w-[3rem]'>
+                        <button
+                          role='button'
+                          type='button'
+                          onClick={() => handleDelete(vape.id)}
+                        >
+                          <ImageWithFallback
+                            src={trash}
+                            alt='delete'
+                            width={20}
+                            height={20}
+                          />
+                        </button>
+                      </td>
+                    </tr>
+                  )
                 )
-              )
-            })}
-        </tbody>
-      </table>
+              })}
+          </tbody>
+        </table>
+      </div>
+      <p className='text-lg font-bold text-center'>
+        <strong className='text-xl'>Total:</strong>{' '}
+        {getCurrencyEUR(
+          vapesToBox
+            .filter((vape) => vape.brand.id === brandId)
+            .reduce((acc, vape) => acc + vape.price * vape.quantity, 0),
+        )}
+      </p>
     </div>
   )
 }
