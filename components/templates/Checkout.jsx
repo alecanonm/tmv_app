1304cce4f936bcd8f27eae96c66076edc148bfea
Jair Cañon <jhcanonn@gmail.com>
2023-12-23 10:26:54 -0500

    Festure: Mobile Checkout CSS

--- components/templates/Checkout.jsx
+++ components/templates/Checkout.jsx
@@ -21,7 +21,7 @@ const Checkout = () => {
         <div className='w-full' />
       </div>
       <div className='md:container md:m-auto md:px-6 flex flex-col md:flex-row gap-2 md:gap-16 lg:gap-20 xl:gap-40 grow absolute inset-0 h-screen'>
-        <div className='w-full flex md:justify-end lg:pr-5 max-md:bg-brandCheckout max-md:px-6 max-md:pt-10'>
+        <div className='w-full flex md:justify-end lg:pr-5 max-md:bg-brandCheckout max-md:px-6 max-md:pt-10 grow'>
           <VapesToPay orderProducts={orderProducts} />
         </div>
         <div className='w-full flex items-center md:overflow-y-auto order-first md:order-last max-md:px-6'>
